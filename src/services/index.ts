47ab2f2b107f2d7f570870729efb1e2ba2f8bcd4
Diego Santelices <dsantelices@midasconsultores.com.ar>
2025-02-18 15:21:14 -0300

    fix

--- src/services/index.ts
+++ src/services/index.ts
@@ -16,6 +16,9 @@ export const getApi = async ({
       method,
       headers: {
         'Content-Type': contentType,
+        'Cache-Control': 'no-store, no-cache, must-revalidate, max-age=0',
+        Pragma: 'no-cache',
+        Expires: '0',
       },
       url: `${apiBase}${path}${query}`,
       data,
