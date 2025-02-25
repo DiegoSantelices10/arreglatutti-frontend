1764bb7bfc317a362bcfc167009ec8b59714e030
Diego Santelices <dsantelices@midasconsultores.com.ar>
2025-02-25 17:47:38 -0300

    fix header

--- src/app/(web)/(home)/layout.tsx
+++ src/app/(web)/(home)/layout.tsx
@@ -1,15 +1,14 @@
 import Header from '@/components/Header';
 import Footer from '@/components/Footer';
 import { Toaster } from '@/components/ui/toaster';
-import { getSession } from '@/actions';
 
 export default async function Layout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
-  const { isLoggedIn } = await getSession();
+  // const { isLoggedIn } = await getSession();
   return (
     <div className="min-h-screen grid content-center">
-      <Header isLoggedIn={isLoggedIn} />
+      <Header />
       {children}
       <Toaster />
       <Footer />
