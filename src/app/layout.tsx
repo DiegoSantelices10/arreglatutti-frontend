ec35dd2dc11ea76735e9e9024bc8609b9b32e303
Diego Santelices <dsantelices@midasconsultores.com.ar>
2025-04-07 13:07:40 -0300

    update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import '../app/globals.css';
 import { cn } from '@/utils';
+import Script from 'next/script';
 import ScrollToTop from '@/components/custom/ScrollToTop';
 
 const inter = Inter({ subsets: ['latin'] });
@@ -18,7 +19,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <meta name="aquiles" content="Aquiles" />
+      <head>
+        <Script id="kommo-widget" strategy="afterInteractive">
+          {`(function(a,m,o,c,r,m){a[m]={id:"1037483",hash:"5a1566db228513a3d4bd578341e16744d7489d7a7d543e91fb3f0ac2db160eb9",locale:"es",setMeta:function(p){this.params=(this.params||[]).concat([p])}};a[o]=a[o]||function(){(a[o].q=a[o].q||[]).push(arguments)};var d=a.document,s=d.createElement('script');s.async=true;s.id=m+'_script';s.src='https://gso.kommo.com/js/button.js';d.head&&d.head.appendChild(s)}(window,0,'crmPlugin',0,0,'crm_plugin'));`}
+        </Script>
+        <meta name="aquiles" content="Aquiles" />
+      </head>
       <body className={cn('bg-primary', inter.className)}>
         {children}
         <ScrollToTop />
