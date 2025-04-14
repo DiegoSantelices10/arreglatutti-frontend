fb35f30b34e83366bda30c56f1faad59fccc39c1
Diego Santelices <dsantelices@midasconsultores.com.ar>
2025-04-14 17:30:01 -0300

    pruebas 1

--- src/app/(web)/(home)/page.tsx
+++ src/app/(web)/(home)/page.tsx
@@ -4,15 +4,17 @@ import Features from '@/components/Features';
 // import Testimonials from '@/components/Testimonials';
 import HomeUI from '@/components/Home';
 import { getProfessions } from '@/services/profesion';
+import HomeNotification from '@/components/PushNotificationManager';
 
 const Home: FC = async () => {
   const { data } = await getProfessions();
 
   return (
     <>
-      <ScrollUp />
+      <HomeNotification />
+      {/* <ScrollUp />
       <HomeUI professions={data} />
-      <Features />
+      <Features /> */}
     </>
   );
 };
