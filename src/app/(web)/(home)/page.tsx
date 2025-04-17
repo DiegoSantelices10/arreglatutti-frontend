a89e59e3bc7552970e03d54fc95b5669e961d31f
Diego Santelices <dsantelices@midasconsultores.com.ar>
2025-04-17 10:07:17 -0300

    cambios a dev

--- src/app/(web)/(home)/page.tsx
+++ src/app/(web)/(home)/page.tsx
@@ -4,14 +4,12 @@ import Features from '@/components/Features';
 // import Testimonials from '@/components/Testimonials';
 import HomeUI from '@/components/Home';
 import { getProfessions } from '@/services/profesion';
-// import HomeNotification from '@/components/PushNotificationManager';
 
 const Home: FC = async () => {
   const { data } = await getProfessions();
 
   return (
     <>
-      {/* <HomeNotification /> */}
       <ScrollUp />
       <HomeUI professions={data} />
       <Features />
--- src/components/PushNotificationManager/index.tsx
+++ src/components/PushNotificationManager/index.tsx
@@ -1,105 +1,9 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 'use client';
-
-import {
-  sendNotification,
-  subscribeUser,
-  unsubscribeUser,
-} from '@/app/actions';
 import { useState, useEffect } from 'react';
+import Button from '../custom/Button';
 
-function urlBase64ToUint8Array(base64String: string) {
-  const padding = '='.repeat((4 - (base64String.length % 4)) % 4);
-  const base64 = (base64String + padding).replace(/-/g, '+').replace(/_/g, '/');
-
-  const rawData = window.atob(base64);
-  const outputArray = new Uint8Array(rawData.length);
-
-  for (let i = 0; i < rawData.length; ++i) {
-    outputArray[i] = rawData.charCodeAt(i);
-  }
-  return outputArray;
-}
-
-function PushNotificationManager() {
-  const [isSupported, setIsSupported] = useState(false);
-  const [subscription, setSubscription] = useState<PushSubscription | null>(
-    null
-  );
-  const [message, setMessage] = useState('');
-
-  useEffect(() => {
-    if ('serviceWorker' in navigator && 'PushManager' in window) {
-      setIsSupported(true);
-      registerServiceWorker();
-    }
-  }, []);
-
-  async function registerServiceWorker() {
-    const registration = await navigator.serviceWorker.register('/sw.js', {
-      scope: '/',
-      updateViaCache: 'none',
-    });
-    const sub = await registration.pushManager.getSubscription();
-    setSubscription(sub);
-  }
-
-  async function subscribeToPush() {
-    const registration = await navigator.serviceWorker.ready;
-    const sub = await registration.pushManager.subscribe({
-      userVisibleOnly: true,
-      applicationServerKey: urlBase64ToUint8Array(
-        process.env.NEXT_PUBLIC_VAPID_PUBLIC_KEY!
-      ),
-    });
-    setSubscription(sub);
-    const serializedSub = JSON.parse(JSON.stringify(sub));
-    await subscribeUser(serializedSub);
-  }
-
-  async function unsubscribeFromPush() {
-    await subscription?.unsubscribe();
-    setSubscription(null);
-    await unsubscribeUser();
-  }
-
-  async function sendTestNotification() {
-    if (subscription) {
-      await sendNotification(message);
-      setMessage('');
-    }
-  }
-
-  if (!isSupported) {
-    return <p>Push notifications are not supported in this browser.</p>;
-  }
-
-  return (
-    <div>
-      <h3>Push Notifications</h3>
-      {subscription ? (
-        <>
-          <p>You are subscribed to push notifications.</p>
-          <button onClick={unsubscribeFromPush}>Unsubscribe</button>
-          <input
-            type="text"
-            placeholder="Enter notification message"
-            value={message}
-            onChange={(e) => setMessage(e.target.value)}
-          />
-          <button onClick={sendTestNotification}>Send Test</button>
-        </>
-      ) : (
-        <>
-          <p>You are not subscribed to push notifications.</p>
-          <button onClick={subscribeToPush}>Subscribe</button>
-        </>
-      )}
-    </div>
-  );
-}
-
-function InstallPrompt() {
+const InstallPrompt = () => {
   const [deferredPrompt, setDeferredPrompt] = useState<any>(null);
   const [showInstall, setShowInstall] = useState(false);
 
@@ -131,20 +35,13 @@ function InstallPrompt() {
   };
 
   return showInstall ? (
-    <div>
-      <h3>Instalar App</h3>
-      <button onClick={handleInstallClick}>
-        Agregar a la pantalla de inicio
-      </button>
+    <div className="absolute top-4 right-2">
+      <div className="flex gap-1">
+        <h3>Descarga nuestra app!</h3>
+        <Button onClick={handleInstallClick}>Instalar</Button>
+      </div>
     </div>
   ) : null;
-}
+};
 
-export default function HomeNotification() {
-  return (
-    <div className="h-screen bg-white grid conten">
-      <PushNotificationManager />
-      <InstallPrompt />
-    </div>
-  );
-}
+export default InstallPrompt;
